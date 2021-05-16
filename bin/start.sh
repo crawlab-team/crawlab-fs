310a54e3a37612e258c8ffcd713d6d494c524d17
marvzhang <tikazyq@163.com>
2021-05-16 16:28:46 +0800

    updated workflow

--- bin/start.sh
+++ bin/start.sh
@@ -4,10 +4,21 @@ if [ -e ./tmp ]; then
 else
   mkdir ./tmp
 fi
-./seaweedfs/weed server \
-  -dir ./tmp \
-  -master.dir ./tmp \
-  -volume.dir.idx ./tmp \
-  -ip localhost \
-  -ip.bind 0.0.0.0 \
-  -filer
+
+if [ -x /usr/local/bin/weed ]; then
+  weed server \
+    -dir ./tmp \
+    -master.dir ./tmp \
+    -volume.dir.idx ./tmp \
+    -ip localhost \
+    -ip.bind 0.0.0.0 \
+    -filer
+else
+  ./seaweedfs/weed server \
+    -dir ./tmp \
+    -master.dir ./tmp \
+    -volume.dir.idx ./tmp \
+    -ip localhost \
+    -ip.bind 0.0.0.0 \
+    -filer
+fi
